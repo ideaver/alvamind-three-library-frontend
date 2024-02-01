ad7a9579455439f149c944d0e7dcb63483faad2e
dsaauser1 <dsaauser1@gmail.com>
2024-02-02 06:33:06 +0700

    refactor: :art: Updated AppCheckbox

--- lib/widget/molecule/app_checkbox.dart
+++ lib/widget/molecule/app_checkbox.dart
@@ -8,11 +8,13 @@ class AppCheckbox extends StatelessWidget {
   final bool enable;
   final bool? value;
   final String? title;
+  final Widget? titleWidget;
   final String? errorText;
   final Color activeColor;
   final Color? fillColor;
   final TextStyle? titleStyle;
   final EdgeInsets padding;
+  final CrossAxisAlignment crossAxisAlignment;
   final Function(bool?) onChanged;
 
   const AppCheckbox({
@@ -20,11 +22,13 @@ class AppCheckbox extends StatelessWidget {
     this.enable = true,
     required this.value,
     this.title,
+    this.titleWidget,
     this.errorText,
     this.activeColor = AppColors.primary,
     this.fillColor,
     this.titleStyle,
     this.padding = EdgeInsets.zero,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
     required this.onChanged,
   });
 
@@ -40,52 +44,76 @@ class AppCheckbox extends StatelessWidget {
           children: [
             Row(
               mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: crossAxisAlignment,
               children: [
-                Checkbox(
-                  value: value,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(AppSizes.radius / 1.5),
-                  ),
-                  onChanged: (val) {
-                    if (enable) {
-                      onChanged(val);
-                    }
-                  },
-                  fillColor: errorText != null
-                      ? MaterialStateColor.resolveWith((states) => AppColors.redLv4)
-                      : fillColor != null
-                          ? MaterialStateColor.resolveWith((states) => fillColor!)
-                          : null,
-                  activeColor: activeColor,
-                  side: const BorderSide(width: 1.5, color: AppColors.blackLv6),
-                ),
-                title != null
-                    ? Flexible(
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 4),
-                          child: Text(
-                            title!,
-                            style: titleStyle ??
-                                AppTextStyle.bodyMedium(
-                                  fontWeight: AppFontWeight.semibold,
-                                ),
-                          ),
-                        ),
-                      )
-                    : const SizedBox.shrink(),
+                checkBoxWidget(),
+                titleTextWidget(),
               ],
             ),
-            errorText != null
-                ? Padding(
-                    padding: const EdgeInsets.only(left: AppSizes.padding / 2),
-                    child: Text(
-                      errorText!,
-                      style: AppTextStyle.regular(size: 11, color: AppColors.error),
-                    ),
-                  )
-                : const SizedBox.shrink()
+            errorTextWidget(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget checkBoxWidget() {
+    return Checkbox(
+      value: value,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(AppSizes.radius / 1.5),
+      ),
+      visualDensity: VisualDensity.compact,
+      onChanged: (val) {
+        if (enable) {
+          onChanged(val);
+        }
+      },
+      fillColor: errorText != null
+          ? MaterialStateColor.resolveWith((states) => AppColors.redLv4)
+          : fillColor != null
+              ? MaterialStateColor.resolveWith((states) => fillColor!)
+              : null,
+      activeColor: activeColor,
+      side: const BorderSide(width: 1.5, color: AppColors.blackLv6),
+    );
+  }
+
+  Widget titleTextWidget() {
+    if (titleWidget != null) {
+      return Flexible(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 4),
+          child: titleWidget!,
+        ),
+      );
+    }
+
+    if (title == null) {
+      return const SizedBox.shrink();
+    }
+
+    return Flexible(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 4),
+        child: Text(
+          title!,
+          style: titleStyle ?? AppTextStyle.bodyMedium(fontWeight: AppFontWeight.semibold),
+        ),
+      ),
+    );
+  }
+
+  Widget errorTextWidget() {
+    if (errorText == null) {
+      return const SizedBox.shrink();
+    }
+
+    return Padding(
+      padding: const EdgeInsets.only(left: AppSizes.padding / 2),
+      child: Text(
+        errorText!,
+        style: AppTextStyle.regular(size: 11, color: AppColors.error),
       ),
     );
   }
