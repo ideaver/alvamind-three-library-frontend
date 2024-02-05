25ff1c8aa44f1df6b820b375ea7a5bc3bbe4d410
dsaauser1 <dsaauser1@gmail.com>
2024-02-05 09:36:34 +0700

    refactor: :art: Updated AppInkwell

--- lib/widget/atom/app_inkwell.dart
+++ lib/widget/atom/app_inkwell.dart
@@ -1,3 +1,4 @@
+import 'package:alvamind_three_library_frontend/app/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 import '../../app/theme/app_sizes.dart';
@@ -5,28 +6,37 @@ import '../../app/theme/app_sizes.dart';
 class AppInkwell extends StatelessWidget {
   final double? width;
   final double? height;
+  final double borderWidth;
   final BorderRadius? borderRadius;
+  final EdgeInsets? margin;
   final EdgeInsets padding;
   final Color? color;
+  final Color borderColor;
   final List<BoxShadow>? boxShadow;
   final Widget? child;
+  final BoxBorder? customBorder;
   final Function()? onTap;
 
   const AppInkwell({
     super.key,
     this.width,
     this.height,
+    this.borderWidth = 1,
     this.borderRadius,
     this.padding = const EdgeInsets.all(AppSizes.padding),
+    this.margin = const EdgeInsets.all(AppSizes.padding),
     this.color,
+    this.borderColor = AppColors.blackLv8,
     this.boxShadow,
     this.child,
+    this.customBorder,
     this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: margin,
       decoration: BoxDecoration(
         borderRadius: borderRadius ?? BorderRadius.circular(AppSizes.radius),
         boxShadow: boxShadow,
@@ -45,6 +55,11 @@ class AppInkwell extends StatelessWidget {
             padding: padding,
             decoration: BoxDecoration(
               color: color,
+              border: customBorder ??
+                  Border.all(
+                    width: borderWidth,
+                    color: borderColor,
+                  ),
               borderRadius: borderRadius ?? BorderRadius.circular(AppSizes.radius),
             ),
             child: child,
